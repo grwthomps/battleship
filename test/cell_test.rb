deaf89f1a550353891e97a429ce2bec26ef2396a
Tyler Balogh <tyler_balogh@yahoo.com>
2019-08-21 14:05:07 -0600

    Create last five iteration 1 test for cell class
    Co-authored-by: Graham Thompson <51726839+grwthomps@users.noreply.github.com>

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -8,6 +8,7 @@ class CellTest < Minitest::Test
   def setup
     @cell = Cell.new("B4")
     @cruiser = Ship.new("Cruiser", 3)
+
   end
 
   def test_it_exists
@@ -48,4 +49,37 @@ class CellTest < Minitest::Test
     assert_equal true, @cell.fired_upon?
   end
 
+  def test_render_when_it_hasnt_been_fired_upon
+    cell_1 = Cell.new("B4")
+    assert_equal ".", cell_1.render
+  end
+
+  def test_render_when_shot_missed
+    cell_1 = Cell.new("B4")
+    cell_1.fire_upon
+    assert_equal "M", cell_1.render
+  end
+
+  def test_render_when_it_has_a_ship
+    cell_2 = Cell.new("C3")
+    cell_2.place_ship(@cruiser)
+    assert_equal ".", cell_2.render
+    assert_equal "S", cell_2.render(true)
+  end
+
+  def test_render_when_shot_hit
+    cell_2 = Cell.new("C3")
+    cell_2.place_ship(@cruiser)
+    cell_2.fire_upon
+    assert_equal "H", cell_2.render
+    assert_equal false, @cruiser.sunk?
+  end
+
+  def test_render_when_ship_sunk
+    cell_2 = Cell.new("C3")
+    cell_2.place_ship(@cruiser)
+    3.times {@cruiser.hit}
+    assert_equal true, @cruiser.sunk?
+    assert_equal "X", cell_2.render
+  end
 end
