4a8f9620ff628006b3fd2fcabc098554f8a593d3
Graham Thompson <grwthomps@gmail.com>
2019-08-21 13:14:42 -0600

    Create 3 new methods in cell class, check that cell testing is passing so far
    Co-authored-by: Tyler Balogh <52803986+t-balogh@users.noreply.github.com>

--- lib/cell.rb
+++ lib/cell.rb
@@ -11,4 +11,16 @@ class Cell
     @ship == nil
   end
 
+  def place_ship(ship)
+    @ship = ship
+  end
+
+  def fired_upon?
+    @ship.health < @ship.length
+  end
+
+  def fire_upon
+    @ship.hit
+  end
+
 end
--- test/cell_test.rb
+++ test/cell_test.rb
@@ -7,7 +7,7 @@ class CellTest < Minitest::Test
 
   def setup
     @cell = Cell.new("B4")
-    # @cruiser = Ship.new("Cruiser", 3)
+    @cruiser = Ship.new("Cruiser", 3)
   end
 
   def test_it_exists
@@ -29,23 +29,23 @@ class CellTest < Minitest::Test
   def test_an_instance_of_ship
     assert_instance_of Ship, @cruiser
   end
-  
-  # def test_placing_a_ship_on_it
-  #   @cell.place_ship(@cruiser)
-  #   assert_equal @cruiser, @cell.ship
-  #   assert_equal false, @cell.empty?
-  # end
-  #
-  # def test_it_hasnt_been_fired_upon
-  #   @cell.place_ship(@cruiser)
-  #   assert_equal false, @cell.fired_upon?
-  # end
-  #
-  # def test_firing_on_it
-  #   @cell.place_ship(@cruiser)
-  #   @cell.fire_upon
-  #   assert_equal 2, @cell.ship.health
-  #   assert_equal true, @cell.fired_upon?
-  # end
+
+  def test_placing_a_ship_on_it
+    @cell.place_ship(@cruiser)
+    assert_equal @cruiser, @cell.ship
+    assert_equal false, @cell.empty?
+  end
+
+  def test_it_hasnt_been_fired_upon
+    @cell.place_ship(@cruiser)
+    assert_equal false, @cell.fired_upon?
+  end
+
+  def test_firing_on_it
+    @cell.place_ship(@cruiser)
+    @cell.fire_upon
+    assert_equal 2, @cell.ship.health
+    assert_equal true, @cell.fired_upon?
+  end
 
 end
