6d647c0bbbf23799f438801ed58157d3ce07616c
Graham Thompson <grwthomps@gmail.com>
2019-08-20 19:03:16 -0600

    Create cell tests for first section of interaction pattern

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -1,10 +1,41 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/ship'
-require './libg/cell'
-require 'pry'
+require './lib/cell'
 
 class CellTest < Minitest::Test
 
+  def test_it_exists
+    cell = Cell.new("B4")
+    assert_instance_of Cell, cell
+  end
+
+  def test_its_coordinate
+    cell = Cell.new("B4")
+    assert_equal "B4", cell.coordinate
+  end
+
+  def test_there_isnt_a_ship_on_it
+    cell = Cell.new("B4")
+    assert_nil cell.ship
+  end
+
+  def test_that_its_empty
+    cell = Cell.new("B4")
+    assert_equal true, cell.empty?
+  end
+
+  def test_an_instance_of_ship
+    cruiser = Ship.new("Cruiser", 3)
+    assert_instance_of Ship, cruiser
+  end
+
+  def test_placing_a_ship_on_it
+    cell = Cell.new("B4")
+    cruiser = Ship.new("Cruiser", 3)
+    cell.place_ship(cruiser)
+    assert_equal cruiser, cell.ship
+    assert_equal false, cell.empty?
+  end
 
 end
