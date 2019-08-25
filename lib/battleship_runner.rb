5991863642d1be3518d1a38678925f3f53bef125
Graham Thompson <grwthomps@gmail.com>
2019-08-24 18:11:55 -0600

    Create game class with methods for computer placing ships and start work on round method, create runner file and game test file
    Co-authored-by: Tyler Balogh <52803986+t-balogh@users.noreply.github.com>

---
+++ lib/battleship_runner.rb
@@ -0,0 +1,19 @@
+require './lib/ship'
+require './lib/cell'
+require './lib/board'
+require './lib/game'
+
+puts "Welcome to BATTLESHIP"
+puts "Enter p to play. Enter q to quit."
+print "> "
+
+user_input = gets.chomp.downcase
+
+if user_input == "p"
+  game = Game.new
+  game.round
+elsif user_input == "q"
+  # close program
+else
+  puts "Invalid input. Closing program."
+end
