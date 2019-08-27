12f11a165e0aeb88a776c7d50b3125b754be98d7
Tyler Balogh <tyler_balogh@yahoo.com>
2019-08-27 14:59:29 -0600

    Create end method in game file, verify game is working
    Co-authored-by: Graham Thompson <51726839+grwthomps@users.noreply.github.com>

--- lib/battleship_runner.rb
+++ lib/battleship_runner.rb
@@ -4,20 +4,24 @@ require './lib/board'
 require './lib/game'
 
 puts "Welcome to BATTLESHIP"
-puts "Enter p to play. Enter q to quit."
-print "> "
 
-user_input = gets.chomp.downcase
+loop do
+  puts "Enter p to play. Enter q to quit."
+  print "> "
 
-if user_input == "p"
-  game = Game.new
-  game.placing_ships
-  game.turn
-  until game.user_board.render.count("X") == 5 || game.computer_board.render.count("X") == 5 do
+  user_input = gets.chomp.downcase
+
+  if user_input == "p"
+    game = Game.new
+    game.placing_ships
     game.turn
+    until game.user_board.render.count("X") == 5 || game.computer_board.render.count("X") == 5 do
+      game.turn
+    end
+    game.end_game
+  elsif user_input == "q"
+    break
+  else
+    puts "Invalid input. Please try again."
   end
-elsif user_input == "q"
-  # close program
-else
-  puts "Invalid input. Closing program."
 end
